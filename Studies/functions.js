4d12d99432eb3a54164e8c27268f736bcd39f1ec
b.chau504 <brianchau7979@gmail.com>
2022-03-22 17:51:00 +0000

    studies functions

--- Studies/functions.js
+++ Studies/functions.js
@@ -1,4 +1,4 @@
-/**
+ /**
  * FUNCTIONS:
  * 0. Functions are a block of code that performs a certain task. 
  * 1. The use of function is divided into two stages. We must first declare or define a function before we can build it. 
@@ -7,5 +7,12 @@
  * 0. Parameters are rather declared variables specifically for that function, that is hoisted to the top on the function scope. 
  * 1. A single output is returned by the function and returns undefined by default. Otherwise, we may use the return keyword followed by a value. When the code block 
  * reaches a return statement, the function will stop executing. 
- * 2. The second stage when using a function, is the invocation or function call.  
+ * 2. Functions can be named, assigned to variables or constants, or be anonymous.  
+ * 3. The second stage when using a function, is the invocation or function call. This will invoke the function to execute or run its code block. 
+ * When we call a function we can pass in arguments to use within the function. The arguments give the parameters values to use. To call a function, we simply reference
+ * the funcion name or variable name that it is assisgned to. We then pass any necessary arguments in paranthesis. 
+ * 4. There are two types of scopes: global and local. Local scopes are local to a function body, whereas global scopes are everything outside of the function. 
+ * Functions, in general, enclose a scope and protect its variables from parent scopes, such as the global scope. Variables in the parent scope can be accessed by 
+ * functions. Parent scope, on the other hand, are unable to access variables in child scopes.
+ * 5.      
  */
