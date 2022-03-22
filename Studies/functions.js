c102bef4f093c0f4874fd4dd32ec7104a414edcd
b.chau504 <brianchau7979@gmail.com>
2022-03-22 16:27:16 +0000

    studies functions

---
+++ Studies/functions.js
@@ -0,0 +1,11 @@
+/**
+ * FUNCTIONS:
+ * 0. Functions are a block of code that performs a certain task. 
+ * 1. The use of function is divided into two stages. We must first declare or define a function before we can build it. 
+ * The keyword function is used to declare a function. Following that are parenthesis, which will hold a number of required inputs separated by commas.
+ * These inputs are called parameters and they should be named accordingly so that it's clear what must be provided when the function is performed. 
+ * 0. Parameters are rather declared variables specifically for that function, that is hoisted to the top on the function scope. 
+ * 1. A single output is returned by the function and returns undefined by default. Otherwise, we may use the return keyword followed by a value. When the code block 
+ * reaches a return statement, the function will stop executing. 
+ * 2. The second stage when using a function, is the invocation or function call.  
+ */
